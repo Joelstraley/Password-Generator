51dd62984991735db0018e5cece6b00f94179180
Joel Straley <Joelstraley@gmail.com>
2020-11-21 16:42:58 -0500

    Combined Array added and write password function with confirms added

--- script.js
+++ script.js
@@ -1,38 +1,68 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
 var numbers = ["1", "2", "3", "4","5","6","7","8","9","0" ]
 var capitals = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"] 
 var specialChar = ["@","#","$","%","<","^","<","!",":",";","?","{","}","+",",","*"]
 
+var combinedArray = ["A,","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","1","2","3","4","5","6","7","8","9","0","@","#","$","%","<","^","<","!",":",";","?","+",",","*"]; 
+
+
 
 // Write password to the #password input
+
 function writePassword() {
-  var charAmount = prompt("How many characters between 8 and 128 do you need?");
-    if (charAmount >= 8 && charAmount <= 128 ) { 
-      return alert("Please Try Again!");
-    } else {
-      return confirm("Do you want the password to include numbers?");
-    } var Num = prompt("Do you want capital letters?");
-   // if (Num) {
-      // return // 
+  totalChars = parseInt(prompt("How many characters between 8 and 128 do you need?"));
+  if ( totalChars < 8 || totalChars > 128) {
+   totalChars = parseInt(prompt("Must be between 8 and 128"))
+} else {
+  hasNumber = confirm("Does your password need numbers?");
+  hasCharacter = confirm("Does your password need Special Characters?");
+  hasUppercase = confirm("Does your password need Uppercase letters?");
+  hasLowercase = confirm("Does your password need Lowercase letters?");
+};
 
-    }
-
-   
-
-
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  passwordText.value = password;
+if (hasNumber && hasCharacter && hasUppercase && hasLowercase) {
   
+
 }
 
 
 
 
+
+}
+
+  
+// else { 
+//  var numChars = confirm("Do you want the password to include numbers?");
+  //var password = generatePassword();
+  //var passwordText = document.querySelector("#password");
+
+
+
+//function generatePassword(){
+ // } else {
+   // return confirm("Do you want the password to include numbers?");
+     
+//if (hasSpecialChars) {
+//    combinedArray.concat(specialChar);
+//}
+
+//if (hasUppercase) {
+//   combinedArray.concat(upperCase);
+//}
+
+//var passLength = //use another Math.random that uses the min and the max for the interval
+
+//for (var i = 0; i < numChars-1; i++) {
+//   nextChar = mathFloor(Math.random(...nextChar.combinedArray));
+//  password.concat(nextChar);
+//}
+
+//}
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
- 
- 
