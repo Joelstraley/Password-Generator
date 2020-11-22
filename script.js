ed92077bd0d736bcc529de4ea1805f1dd4f64d94
Joel Straley <Joelstraley@gmail.com>
2020-11-21 20:54:34 -0500

    console.log tested that correct variables are being saved to final pw array

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 var generateBtn = document.querySelector("#generate");
 
 var numbers = ["1", "2", "3", "4","5","6","7","8","9","0" ]
-var capitals = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"] 
 var specialChar = ["@","#","$","%","<","^","<","!",":",";","?","{","}","+",",","*"]
 
@@ -28,18 +28,33 @@ function generatePassword() {
     } else if (numChars < 8 || numChars > 128) {
       alert("Must be between 8 and 128");
 }  else {
-  var yesNumber = confirm("Does your password need numbers?");
-  var yesCharacter = confirm("Does your password need Special Characters?");
-  var yesUppercase = confirm("Does your password need Uppercase letters?");
   var yesLowercase = confirm("Does your password need Lowercase letters?");
+  var yesUppercase = confirm("Does your password need Uppercase letters?");
+  var yesNumbers = confirm("Does your password need numbers?");
+  var yesSpecialChar = confirm("Does your password need Special Characters?");
 };
 
-if (yesNumber && yesCharacter && yesUppercase && yesLowercase) {
+if (yesLowercase) {
+  combinedArray.concat(lowerCase);
+} if (yesUppercase) {
+  combinedArray.concat(upperCase);
+} if (yesNumbers) {
+  combinedArray.concat(numbers)
+} if (yesSpecialChar) {
+  combinedArray.concat(specialChar)    
+} console.log(combinedArray)
+
+};
+
+var passLength = ///use another Math.random that uses the min and max for the interval
   
-
-} 
-
+/* for (var i = 0; i < numChars-1; i++) {
+  nextChar = Math.floor(Math.random(combinedArray));
+  password.concat(nextChar);
 }
+
+  */
+
 
 
 
